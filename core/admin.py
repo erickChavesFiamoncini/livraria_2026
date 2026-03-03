35c2b4396e1ee88f64733ccc1e0c2252c18b5dd3
erickChavesFiamoncini <erickechaves@gmail.com>
2026-03-03 15:00:22 -0300

    feat: criação de editoras e inclusão na API

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,3 @@
-"""
-Django admin customization.
-"""
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
@@ -53,3 +49,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Categoria)
+admin.site.register(models.Editora)
